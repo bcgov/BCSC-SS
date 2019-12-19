ab73895b1f823ac2005e776592b2df5a64cce0db
Shabeeb <myselfshabeeb@gmail.com>
2019-12-18 16:04:18 -0800

    added vuetify and updated jest configration

--- selfservice-ui/src/App.vue
+++ selfservice-ui/src/App.vue
@@ -1,24 +1,22 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app>
+    <v-content>
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
 
-@Component({
-  components: {
-    HelloWorld
-  }
-})
+@Component({})
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
