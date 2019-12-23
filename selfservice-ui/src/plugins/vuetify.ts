efc6bd93f3b2550b1175cd8db732c7fa52ec5756
Shabeeb <myselfshabeeb@gmail.com>
2019-12-23 10:44:24 -0800

    updated fonts and bc logo

--- selfservice-ui/src/plugins/vuetify.ts
+++ selfservice-ui/src/plugins/vuetify.ts
@@ -1,8 +1,5 @@
-import '@mdi/font/css/materialdesignicons.css';
-import 'animate.css';
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify);
 
