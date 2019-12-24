d731980a9afd06cf0e48d08d6cee511fe40daafb
Shabeeb <myselfshabeeb@gmail.com>
2019-12-23 16:23:14 -0800

    added footer and its styles

--- selfservice-ui/src/App.vue
+++ selfservice-ui/src/App.vue
@@ -6,29 +6,37 @@
         <router-view />
       </v-container>
     </v-content>
+    <Footer />
   </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Header from '@/components/Header/Header.vue';
+import Footer from '@/components/Footer/Footer.vue';
+
 @Component({
   components: {
-    Header
+    Header,
+    Footer
   }
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import './assets/styles/theme.scss';
+
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  // margin-top: 60px;
 }
 .main-content {
-  margin-top: 60px;
+  margin-top: 10px;
+  @include sm {
+    margin-top: 56px;
+  }
 }
 </style>
