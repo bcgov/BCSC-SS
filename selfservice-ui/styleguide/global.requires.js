052d6e226db8b0ed1abde25b91833e35683debbc
Shabeeb <myselfshabeeb@gmail.com>
2019-12-30 12:08:56 -0800

    added styleguidist for style guidlines

---
+++ selfservice-ui/styleguide/global.requires.js
@@ -0,0 +1,6 @@
+import '@mdi/font/css/materialdesignicons.css';
+import 'animate.css';
+import 'vuetify/dist/vuetify.min.css';
+import '@/assets/styles/base.scss';
+import '@/assets/styles/layout.scss';
+import '@/assets/styles/overrides.scss';
