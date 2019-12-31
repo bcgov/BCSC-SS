05c652aa0b45e337766f0d22977275512915ee95
Shabeeb <myselfshabeeb@gmail.com>
2019-12-31 10:28:00 -0800

    updated unit testing for basic component

---
+++ selfservice-ui/tests/unit/App.spec.ts
@@ -0,0 +1,20 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import App from '@/App.vue';
+import vuetify from 'vuetify';
+
+describe('App.vue', () => {
+  let wrapper: any;
+  beforeEach(() => {
+    const localVue = createLocalVue();
+    localVue.use(vuetify);
+
+    wrapper = shallowMount(App, {
+      localVue,
+      stubs: ['router-view']
+    });
+  });
+
+  it('renders props when passed', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
