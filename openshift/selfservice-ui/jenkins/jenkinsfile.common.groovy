3e38fbff51e94379133bb03f40b75609fa235ec6
Kulpreet Singh <kulpreetsingh.acme@gmail.com>
2020-01-14 14:01:16 -0800

    Updated Jenkinsfile path

--- openshift/selfservice-ui/jenkins/jenkinsfile.common.groovy
+++ openshift/selfservice-ui/jenkins/jenkinsfile.common.groovy
@@ -102,7 +102,7 @@ String getUrlForRoute(String routeName, String projectNameSpace = '') {
     nameSpaceFlag = "-n ${projectNameSpace}"
   }
   def url = sh (
-    script: """ 'https://' + $(oc get routes jenkins -o json | jq '.spec.host' | xargs echo) + '/' | echo $url""",
+    script: """ 'https://' + `oc get routes jenkins -o json | jq '.spec.host' | xargs echo` + '/' | echo $url""",
     returnStdout: true
   ).trim()
 
