4b07035ca2a2ac4a68fb318502b9bc4d1d3bd5bd
Kulpreet Singh <kulpreetsingh.acme@gmail.com>
2020-01-14 14:18:51 -0800

    Updated Jenkinsfile path

--- openshift/selfservice-ui/jenkins/jenkinsfile.web.dev.groovy
+++ openshift/selfservice-ui/jenkins/jenkinsfile.web.dev.groovy
@@ -13,9 +13,6 @@ TOOLS_TAG = 'tools'
 NAMESPACE_BUILD = "${NAMESPACE}"  + '-' + "${TOOLS_TAG}"
 ROCKETCHAT_CHANNEL='#bcsc-ss-bot'
 
-ROCKETCHAT_TOKEN = sh (
-                    script: """oc get secret/rocketchat-token-secret -n ${NAMESPACE_BUILD} -o template --template="{{.data.ROCKETCHAT_TOKEN}}" | base64 --decode""",
-                        returnStdout: true).trim()
 
 stage('Build ' + common.APP_NAME) {
   node{
@@ -26,6 +23,10 @@ stage('Build ' + common.APP_NAME) {
 
         // Build and verify the app
         common.buildAndVerify(WEB_BUILD)
+
+        ROCKETCHAT_TOKEN = sh (
+                    script: """oc get secret/rocketchat-token-secret -n ${NAMESPACE_BUILD} -o template --template="{{.data.ROCKETCHAT_TOKEN}}" | base64 --decode""",
+                        returnStdout: true).trim()
 
         rocketChatNotificaiton("${ROCKETCHAT_TOKEN}", "${ROCKETCHAT_CHANNEL}", '{"username":"bcsc-jedi","icon_url":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTizwY92yvdrPaFVBlbw6JW9fiDxZrogj10UvkKGnp66xLNx3io5Q&s","text":"SelfService-UI build Success","attachments":[{"title":"Selfservice-ui build","title_link":"${currentBuild.absoluteUrl}/console","text":"Selfservice-ui build details:","image_url":"https://i.imgflip.com/1czxka.jpg","color":"#764FA5"}]}' )
         // Don't tag with BUILD_ID so the pruner can do it's job; it won't delete tagged images.
@@ -85,6 +86,9 @@ stage("Deploy to ${common.web_environments.dev.name}") {
   node{
     try{
       common.deployAndVerify(IMAGE_HASH,environment,IMAGESTREAM_NAME)
+      ROCKETCHAT_TOKEN = sh (
+                    script: """oc get secret/rocketchat-token-secret -n ${NAMESPACE_BUILD} -o template --template="{{.data.ROCKETCHAT_TOKEN}}" | base64 --decode""",
+                        returnStdout: true).trim()
       rocketChatNotificaiton("${ROCKETCHAT_TOKEN}", "${ROCKETCHAT_CHANNEL}", '{"username":"bcsc-jedi","icon_url":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTizwY92yvdrPaFVBlbw6JW9fiDxZrogj10UvkKGnp66xLNx3io5Q&s","text":"SelfService-UI Deployment Success","attachments":[{"title":"Selfservice-ui Deployment","title_link":"${currentBuild.absoluteUrl}/console","text":"Selfservice-ui build details:","image_url":"https://i.imgflip.com/1czxka.jpg","color":"#764FA5"}]}' )
     //   common.notifyNewDeployment(environment,url,"Deploy to ${common.environments.test.name}?")
     }catch(error){
