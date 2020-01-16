2d511ccfec91db5e5745c6009cb90d0d56140d25
Kulpreet Singh <kulpreetsingh.acme@gmail.com>
2020-01-15 21:39:15 -0800

    Testing pipeline issues

--- jenkins/jenkinsfile.common.groovy
+++ jenkins/jenkinsfile.common.groovy
@@ -122,13 +122,12 @@ def deployAndVerify(srcHash, destination, imageStream){
 
 @NonCPS
 def rocketChatNotificaiton(token, channel, app_name) {
-//   def payload = JsonOutput.toJson([text: comments, channel: channel])
   def rocketChatUrl = "https://chat.pathfinder.gov.bc.ca/hooks/" + "${token}"
   build_url = "${currentBuild.absoluteUrl}/console"
-  COMMENT = "{"username":"bcsc-jedi","icon_url":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTizwY92yvdrPaFVBlbw6JW9fiDxZrogj10UvkKGnp66xLNx3io5Q&s","text":"${app_name} Deployment Success 🚀","attachments":[{"title":"${app_name} Deployment","title_link":"${build_url}","text":"${app_name} Deployment details:","image_url":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwc_SWm-J_9OPSJVzUqxibPHZI55EBwpOB-JPeY0drU64YENdUWA&s","color":"#1ee321"}]}"
-//   COMMENT = "${comments}"
+  COMMENT = '{"username":"bcsc-jedi","icon_url":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTizwY92yvdrPaFVBlbw6JW9fiDxZrogj10UvkKGnp66xLNx3io5Q&s","text":"${app_name} Deployment Success 🚀","attachments":[{"title":"${app_name} Deployment","title_link":"${build_url}","text":"${app_name} Deployment details:","image_url":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwc_SWm-J_9OPSJVzUqxibPHZI55EBwpOB-JPeY0drU64YENdUWA&s","color":"#1ee321"}]}'
+  def payload = JsonOutput.toJson([text: COMMENT, channel: channel])
   sh(returnStdout: true,
-     script: "curl -X POST -H 'Content-Type: application/json' --data \'${COMMENT}\' ${rocketChatUrl}")
+     script: "curl -X POST -H 'Content-Type: application/json' --data \'${payload}\' ${rocketChatUrl}")
 }
 
 // def notifyGood(title,description,buttons=[]){
