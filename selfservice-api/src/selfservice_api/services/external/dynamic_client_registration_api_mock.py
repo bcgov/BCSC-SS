003e263fc1059a6e62b441ae5b57d0cc90debac7
Vysakh Menon <vysakh.menon@aot-technologies.com>
2020-01-22 15:01:42 -0800

    sonarcloud property

--- selfservice-api/src/selfservice_api/services/external/dynamic_client_registration_api_mock.py
+++ selfservice-api/src/selfservice_api/services/external/dynamic_client_registration_api_mock.py
@@ -11,10 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""***Temperory***: API mock for BCSC OIDC Dynamic Client Registration.
-
-@sonar.issue.ignore.allfile
-"""
+"""***Temperory***: API mock for BCSC OIDC Dynamic Client Registration."""
 
 import binascii
 import json
