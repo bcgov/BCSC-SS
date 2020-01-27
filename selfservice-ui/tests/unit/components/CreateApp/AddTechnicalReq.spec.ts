28d3313c8827112c8fd9966f462de491e6335b79
Shabeeb <myselfshabeeb@gmail.com>
2020-01-27 14:39:20 -0800

    updated unit test cases

--- selfservice-ui/tests/unit/components/CreateApp/AddTechnicalReq.spec.ts
+++ selfservice-ui/tests/unit/components/CreateApp/AddTechnicalReq.spec.ts
@@ -1,42 +1,86 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
 import AddTechnicalReq from '@/components/CreateApp/AddTechnicalReq.vue';
-import vuetify from 'vuetify';
+import Vuetify from 'vuetify';
 import Vuex from 'vuex';
 
 describe('AddTechnicalReq.vue', () => {
-  let wrapper: any;
-  beforeEach(() => {
-    const localVue = createLocalVue();
-    localVue.use(vuetify);
-    localVue.use(Vuex);
-    const store = new Vuex.Store({
-      modules: {
-        ProjectInfoModule: {
-          namespaced: true,
-          state: {},
-          getters: {
-            getSingleProjectInfo: jest.fn(),
-            isLoggedin: jest.fn()
-          },
-          actions: { getSingleTechnicalReq: jest.fn() }
+  let vuetify: any;
+  const localVue = createLocalVue();
+
+  localVue.use(Vuetify);
+  localVue.use(Vuex);
+  vuetify = new Vuetify();
+  const store = new Vuex.Store({
+    modules: {
+      ProjectInfoModule: {
+        namespaced: true,
+        state: {},
+        getters: {
+          getSingleProjectInfo: jest.fn(),
+          isLoggedin: jest.fn()
         },
-        TechnicalReqModule: {
-          namespaced: true,
-          state: {},
-          getters: {
-            getSingleTechnicalReq: jest.fn()
-          },
-          actions: { getSingleTechnicalReq: jest.fn() }
+        actions: {
+          getSingleTechnicalReq: jest.fn(),
+          loadSingleProjectInfo: jest.fn()
         }
+      },
+      TechnicalReqModule: {
+        namespaced: true,
+        state: {},
+        getters: {
+          getSingleTechnicalReq: jest.fn()
+        },
+        actions: { getSingleTechnicalReq: jest.fn() }
       }
-    });
-    wrapper = shallowMount(AddTechnicalReq, {
-      localVue,
-      store
-    });
+    }
   });
 
+  const mountFunction = (options: any) => {
+    return mount(AddTechnicalReq, {
+      store,
+      vuetify,
+      localVue,
+      ...options
+    });
+  };
+
+  // // let wrapper: any;
+  // beforeEach(() => {
+  //   const localVue = createLocalVue();
+  //   localVue.use(vuetify);
+  //   localVue.use(Vuex);
+  //   const store = new Vuex.Store({
+  //     modules: {
+  //       ProjectInfoModule: {
+  //         namespaced: true,
+  //         state: {},
+  //         getters: {
+  //           getSingleProjectInfo: jest.fn(),
+  //           isLoggedin: jest.fn()
+  //         },
+  //         actions: {
+  //           getSingleTechnicalReq: jest.fn(),
+  //           loadSingleProjectInfo: jest.fn()
+  //         }
+  //       },
+  //       TechnicalReqModule: {
+  //         namespaced: true,
+  //         state: {},
+  //         getters: {
+  //           getSingleTechnicalReq: jest.fn()
+  //         },
+  //         actions: { getSingleTechnicalReq: jest.fn() }
+  //       }
+  //     }
+  //   });
+  //   wrapper = shallowMount(AddTechnicalReq, {
+  //     localVue,
+  //     store
+  //   });
+  // });
+
   it('renders props when passed', () => {
+    const wrapper = mountFunction({});
     expect(wrapper.element).toMatchSnapshot();
   });
 });
---
+++ selfservice-ui/tests/unit/components/Header/Sidebar.spec.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Sidebar from '@/components/Header/Sidebar.vue';
+import vuetify from 'vuetify';
+
+describe('Sidebar.vue', () => {
+  let wrapper: any;
+  beforeEach(() => {
+    const localVue = createLocalVue();
+    localVue.use(vuetify);
+
+    wrapper = shallowMount(Sidebar, {
+      localVue
+    });
+  });
+
+  it('renders props when passed', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
