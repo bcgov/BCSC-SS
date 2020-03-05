35602b3b6e818f52e25589bf86b0723544022e9a
shabeeb <myselfshabeeb@gmail.com>
2020-03-04 16:29:49 -0800

    summary page with API key

--- selfservice-ui/src/components/CreateApp/ClientID.vue
+++ selfservice-ui/src/components/CreateApp/ClientID.vue
@@ -22,7 +22,7 @@
           ></span>
         </v-list-item-content>
         <v-list-item-content
-          class="align-end pointer font-weight-bold"
+          class="align-end pointer font-weight-bold client-id-copy"
           @click="
                   docopy(
                     getApiData.oidcConfig && getApiData.oidcConfig.clientId
@@ -96,143 +96,9 @@
               </tbody>
             </template>
           </v-simple-table>
-          <!-- <span class="pl-5">cardNumber ,passcode</span>
-          <div v-for="(account, idx) in getApiData.testAccount" :key="account">
-            <v-icon small class="mr-1" v-if="idx === 0">mdi-account</v-icon>
-            <span :class="idx ===0 ? '' :'pl-5'">{{ account.cardNumber }} , {{ account.passcode }}</span>
-          </div>-->
         </v-list-item-content>
       </v-list-item>
     </v-list>
-    <!-- <v-toolbar flat color="#38598a" dark>
-      <v-card-title>{{ $t('ClientID.pagetitle') }}</v-card-title>
-      <div class="flex-grow-1"></div>
-      <v-col class="col-lg-4 col-md-5 col-8">
-        <v-alert type="error" v-if="errorStatus" class="alert-top">Something went wrong...</v-alert>
-      </v-col>
-      <div class="flex-grow-1"></div>
-    </v-toolbar>
-    <v-divider></v-divider>-->
-
-    <!-- <v-item-group>
-      <v-container>
-        <Loading v-if="isLoading && getApiData === 0" />
-        <v-row class="ma-5" v-else>
-          <v-snackbar v-model="snackbar" :timeout="timeout">
-            {{ text }}
-            <v-btn
-              color="blue"
-              text
-              @click="snackbar = false"
-            >{{ $t('ClientID.labelSnackbarClose') }}</v-btn>
-          </v-snackbar>
-
-          <v-col cols="12" md="12">
-            <v-card flat>
-              <v-list-item-content class="headline">
-                {{
-                $t('ClientID.titleApiKey')
-                }}
-              </v-list-item-content>
-              <v-list-item-content v-html="$t('ClientID.titleApiKeyInfo')"></v-list-item-content>
-            </v-card>
-            <v-item
-              class="client-id"
-              :value="getApiData.oidcConfig && getApiData.oidcConfig.clientId"
-            >
-              <v-card
-                class="d-flex align-center px-5 client-id-copy v-card--link"
-                @click="
-                  docopy(
-                    getApiData.oidcConfig && getApiData.oidcConfig.clientId
-                  )
-                "
-              >
-                <v-list-item two-line>
-                  <v-list-item-action>
-                    <v-list-item-title class="title mb-1">
-                      {{
-                      getApiData.oidcConfig && getApiData.oidcConfig.clientId
-                      }}
-                    </v-list-item-title>
-                  </v-list-item-action>
-                </v-list-item>
-                <v-spacer></v-spacer>
-
-                <div class="display-3 flex-grow-1 text-center mr-5">
-                  <v-icon color="#eae9e9" large>mdi-content-copy</v-icon>
-                </div>
-              </v-card>
-            </v-item>
-          </v-col>
-
-          <v-col cols="12" md="12">
-            <v-card flat>
-              <v-list-item-content class="headline">
-                {{
-                $t('ClientID.titleClientSecret')
-                }}
-              </v-list-item-content>
-              <v-list-item-content v-html="$t('ClientID.titleClientSecretInfo')"></v-list-item-content>
-            </v-card>
-            <v-item
-              class="client-id"
-              :value="
-                getApiData.oidcConfig && getApiData.oidcConfig.clientSecret
-              "
-            >
-              <v-card
-                class="d-flex align-center px-4 client-id v-card--link"
-                @click="
-                  docopy(
-                    getApiData.oidcConfig && getApiData.oidcConfig.clientSecret
-                  )
-                "
-              >
-                <v-list-item two-line>
-                  <v-list-item-content>
-                    <v-list-item-content class="title mb-1">
-                      {{
-                      getApiData.oidcConfig &&
-                      getApiData.oidcConfig.clientSecret
-                      }}
-                    </v-list-item-content>
-                  </v-list-item-content>
-                </v-list-item>
-                <v-spacer></v-spacer>
-
-                <div class="display-3 flex-grow-1 text-center mr-5">
-                  <v-icon color="#eae9e9" large>mdi-content-copy</v-icon>
-                </div>
-              </v-card>
-            </v-item>
-          </v-col>
-          <v-col cols="12" md="12">
-            <v-card flat>
-              <v-list-item-content class="headline">
-                {{
-                $t('ClientID.titleTestAccount')
-                }}
-              </v-list-item-content>
-              <v-list-item-content v-html="$t('ClientID.titleTestAccountInfo')"></v-list-item-content>
-            </v-card>
-            <v-item class="client-id">
-              <v-card class="d-flex align-center pa-4 client-id v-card--link">
-                <v-list-item two-line>
-                  <v-list-item-content v-if="getApiData && getApiData.testAccount">
-                    <v-list-item-title
-                      class="subtitle-1 mb-1"
-                      v-for="account in getApiData.testAccount"
-                    >{{ account.cardNumber }} , {{ account.passcode }}</v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-                <v-spacer></v-spacer>
-              </v-card>
-            </v-item>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-item-group>-->
   </v-card>
 </template>
 <script lang="ts">
