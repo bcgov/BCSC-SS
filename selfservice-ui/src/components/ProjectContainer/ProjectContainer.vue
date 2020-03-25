1bd6248c8e31fd20b0a1e49cc339a04457cce277
shabeeb <myselfshabeeb@gmail.com>
2020-03-25 16:42:02 -0700

    project container tab

--- selfservice-ui/src/components/ProjectContainer/ProjectContainer.vue
+++ selfservice-ui/src/components/ProjectContainer/ProjectContainer.vue
@@ -3,7 +3,7 @@
 <template>
   <v-card class="mx-auto" style="max-width: 80%;">
     <v-toolbar flat class="bc-subtitle" dark>
-      <v-toolbar-title>{{ $t('dashboard.pagetitle') }}</v-toolbar-title>
+      <v-toolbar-title>{{ $t('projectContainer.pagetitle') }}</v-toolbar-title>
       <div class="flex-grow-1"></div>
 
       <div class="flex-grow-1"></div>
@@ -13,9 +13,10 @@
       <v-row class="ma-5">
         <v-col>
           <v-tabs slider-color="d-none">
-            <v-tab class="font-weight-bold">Summary Page</v-tab>
-            <v-tab class="font-weight-bold">Item Two</v-tab>
-            <v-tab class="font-weight-bold">Item Three</v-tab>
+            <v-tab class="font-weight-bold">{{ $t('projectContainer.titleDevSummary') }}</v-tab>
+            <v-tab class="font-weight-bold">{{ $t('projectContainer.titleTeamRoles') }}</v-tab>
+            <v-tab class="font-weight-bold">{{ $t('projectContainer.titleLiveAccessReq') }}</v-tab>
+            <v-tab class="font-weight-bold">{{ $t('projectContainer.titlePrivacy') }}</v-tab>
             <v-tab-item class="custom-tabs-items">
               <v-card flat>
                 <v-card-text>
@@ -25,29 +26,13 @@
             </v-tab-item>
             <v-tab-item class="custom-tabs-items">
               <v-card flat>
-                <v-card-text>
-                  <p>Morbi nec metus. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Nunc sed turpis.</p>
-
-                  <p>Suspendisse feugiat. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In hac habitasse platea dictumst. Fusce ac felis sit amet ligula pharetra condimentum.</p>
-
-                  <p>Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Nam commodo suscipit quam. In consectetuer turpis ut velit. Sed cursus turpis vitae tortor. Aliquam eu nunc.</p>
-
-                  <p>Etiam ut purus mattis mauris sodales aliquam. Ut varius tincidunt libero. Aenean viverra rhoncus pede. Duis leo. Fusce fermentum odio nec arcu.</p>
-
-                  <p
-                    class="mb-0"
-                  >Donec venenatis vulputate lorem. Aenean viverra rhoncus pede. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Fusce commodo aliquam arcu. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi.</p>
-                </v-card-text>
+                <v-card-text>content</v-card-text>
               </v-card>
             </v-tab-item>
-            <v-tab-item>
+            <v-tab-item class="custom-tabs-items">
               <v-card flat>
                 <v-card-text>
-                  <p>Fusce a quam. Phasellus nec sem in justo pellentesque facilisis. Nam eget dui. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In dui magna, posuere eget, vestibulum et, tempor auctor, justo.</p>
-
-                  <p
-                    class="mb-0"
-                  >Cras sagittis. Phasellus nec sem in justo pellentesque facilisis. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nam at tortor in tellus interdum sagittis.</p>
+                  <p>content</p>
                 </v-card-text>
               </v-card>
             </v-tab-item>
