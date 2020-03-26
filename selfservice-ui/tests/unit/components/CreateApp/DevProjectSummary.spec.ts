25b4dbe6f471893d7a20c8706aabf5fe093696cd
shabeeb <myselfshabeeb@gmail.com>
2020-03-26 12:40:58 -0700

    project container style and unit test cases

--- selfservice-ui/tests/unit/components/CreateApp/DevProjectSummary.spec.ts
+++ selfservice-ui/tests/unit/components/CreateApp/DevProjectSummary.spec.ts
@@ -22,7 +22,7 @@ describe('DevProjectSummary.vue', () => {
   });
 
   const mountFunction = (options: any) => {
-    return mount(DevProjectSummary, {
+    return shallowMount(DevProjectSummary, {
       store,
       vuetify,
       mocks: { $t: jest.fn(() => {}) }, // tslint:disable-line
@@ -35,74 +35,4 @@ describe('DevProjectSummary.vue', () => {
 
     expect(DevProjectSummaryPage.element).toMatchSnapshot();
   });
-
-  // it('show modal on click', () => {
-  //   const DevProjectSummary = mountFunction({});
-
-  //   DevProjectSummary.setData({ isLoading: false });
-  //   const button = DevProjectSummary.find('.submit-package');
-  //   DevProjectSummary.vm.$on('action-btn:clicked', jest.fn());
-  //   button.trigger('click');
-  //   expect(DevProjectSummary.element).toMatchSnapshot();
-  // });
 });
-
-// describe('DevProjectSummary.vue', () => {
-//   it('renders props when passed', () => {
-//     const store = new Vuex.Store({
-//       modules: {
-//         ProjectInfoModule: {
-//           namespaced: true,
-//           state: {},
-//           getters: {
-//             getSingleProjectInfo: jest.fn(),
-//             errorStatus: jest.fn(),
-//             getFinalProjectSubmissionStatus: jest.fn()
-//           },
-//           actions: {
-//             loadSingleProjectInfo: jest.fn()
-//           }
-//         },
-//         TechnicalReqModule: {
-//           namespaced: true,
-//           state: {},
-//           getters: {
-//             getTechnicalReq: jest.fn(),
-//             errorStatus: jest.fn()
-//           },
-//           actions: {
-//             loadTechnicalReqDetails: jest.fn()
-//           }
-//         },
-//         PackageAndTestModule: {
-//           namespaced: true,
-//           state: {},
-//           getters: {
-//             successStatus: jest.fn(),
-//             getPackageList: jest.fn()
-//           },
-//           actions: {
-//             loadPackage: jest.fn()
-//           }
-//         },
-//         SharedModule: {
-//           namespaced: true,
-//           state: {},
-//           getters: {
-//             isRedirectFromSummaryPage: jest.fn()
-//           },
-//           actions: {
-//             redirectFromSummaryPage: jest.fn()
-//           }
-//         }
-//       }
-//     });
-//     const DevProjectSummary = shallowMount(DevProjectSummary, {
-//       vuetify,
-//       store,
-//       mocks: { $t: jest.fn(() => {}) } // tslint:disable-line
-//     });
-
-//     expect(DevProjectSummary.element).toMatchSnapshot();
-//   });
-// });
