7cac62279ba0af44c1a465352fd627dfed7a51f3
Vysakh Menon <vysakh.menon@aot-technologies.com>
2020-03-27 17:04:02 -0700

    createRoute

--- selfservice-ui/src/router/index.ts
+++ selfservice-ui/src/router/index.ts
@@ -9,89 +9,67 @@ import PageNotFound from '../views/PageNotFound.vue';
 Vue.use(VueRouter);
 
 const metaAllRoles = { requiresAuth: true, roles: ['ss_admin', 'ss_client'] };
+
+function createRoute(path: string, name: string, component: any, meta: any, props = true) {
+  return {
+    path,
+    name,
+    meta,
+    component,
+    props
+  };
+}
+
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      meta: { hideMenu: true },
-      component: Home,
-      props: true
-    },
-    {
-      path: '/login',
-      name: 'login',
-      meta: metaAllRoles,
-      component: () =>
-        import(/* webpackChunkName: "Authorize" */ '../views/Authorize.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      meta: metaAllRoles,
-      props: true,
-      component: () =>
-        import(/* webpackChunkName: "About" */ '../views/About.vue')
-    },
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      meta: metaAllRoles,
-      props: true,
-      component: () =>
-        import(/* webpackChunkName: "dashboard" */ '../views/Dashboard.vue')
-    },
-    {
-      path: '/profile/:step?',
-      name: 'profile',
-      meta: metaAllRoles,
-      props: true,
-      component: () =>
-        import(/* webpackChunkName: "profile" */ '../views/Profile.vue')
-    },
-    {
-      path: '/project/info',
-      name: 'project-info',
-      meta: metaAllRoles,
-      props: true,
-      component: () =>
-        import(/* webpackChunkName: "project" */ '../views/Project.vue')
-    },
-    {
-      path: '/project/:id?/:step?',
-      name: 'project',
-      meta: metaAllRoles,
-      props: true,
-      component: () =>
-        import(/* webpackChunkName: "project" */ '../views/Project.vue')
-    },
-    {
-      path: '/project-container/:id?/:tab?',
-      name: 'projectcontainer',
-      meta: metaAllRoles,
-      props: true,
-      component: () =>
-        import(
-          /* webpackChunkName: "projectcontainer" */ '../views/ProjectContainer.vue'
-        )
-    },
-    {
-      path: '/add-test-account',
-      name: 'testaccount',
-      meta: { requiresAuth: true, roles: ['ss_admin'] },
-      props: true,
-      component: () =>
-        import(/* webpackChunkName: "testaccount" */ '../views/TestAccount.vue')
-    },
-    {
-      path: '/unauthorized',
-      name: 'Unauthorized',
-      meta: { requiresAuth: false },
-      props: true,
-      component: Unauthorized
-    },
+    createRoute('/', 'home', Home, { hideMenu: true }),
+    createRoute(
+      '/login',
+      'login',
+      () => import(/* webpackChunkName: "Authorize" */ '../views/Authorize.vue'),
+      metaAllRoles),
+    createRoute(
+      '/about',
+      'about',
+      () => import(/* webpackChunkName: "About" */ '../views/About.vue'),
+      metaAllRoles),
+    createRoute(
+      '/dashboard',
+      'dashboard',
+      () => import(/* webpackChunkName: "dashboard" */ '../views/Dashboard.vue'),
+      metaAllRoles),
+    createRoute(
+      '/profile/:step?',
+      'profile',
+      () => import(/* webpackChunkName: "profile" */ '../views/Profile.vue'),
+      metaAllRoles),
+    createRoute(
+      '/project/info',
+      'project-info',
+      () => import(/* webpackChunkName: "project" */ '../views/Project.vue'),
+      metaAllRoles),
+    createRoute(
+      '/project/:id?/:step?',
+      'project',
+      () => import(/* webpackChunkName: "project" */ '../views/Project.vue'),
+      metaAllRoles),
+    createRoute(
+      '/project-container/:id?/:tab?',
+      'projectcontainer',
+      () => import(/* webpackChunkName: "projectcontainer" */ '../views/ProjectContainer.vue'),
+      metaAllRoles),
+    createRoute(
+      '/add-test-account',
+      'testaccount',
+      () => import(/* webpackChunkName: "testaccount" */ '../views/TestAccount.vue'),
+      { requiresAuth: true, roles: ['ss_admin'] }),
+    createRoute(
+      '/unauthorized',
+      'Unauthorized',
+      Unauthorized,
+      { requiresAuth: false }),
     { path: '*', meta: { requiresAuth: false }, component: PageNotFound }
   ]
 });
