4e89532c20247f79150440b0b0da9f4d24e83354
Vysakh Menon <vysakh.menon@aot-technologies.com>
2020-04-09 16:13:52 -0700

    style change in delete popup

--- selfservice-ui/src/components/TeamRoles/TeamRoles.vue
+++ selfservice-ui/src/components/TeamRoles/TeamRoles.vue
@@ -68,14 +68,13 @@
         </div>
       </v-col>
       <v-col>
-        <v-dialog v-model="dialogDelete" persistent max-width="290">
+        <v-dialog v-model="dialogDelete" persistent max-width="400">
           <v-card>
-            <v-card-title>Confirm Delete</v-card-title>
-            <v-card-text>Are you sure you want to delete?</v-card-text>
+            <v-card-text class="text-left">Are you sure you want to delete?</v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn text @click="deleteMember(true)">Cancel</v-btn>
-              <v-btn text @click="deleteMember()">Delete</v-btn>
+              <Button secondary text @click="deleteMember(true)">Cancel</Button>
+              <Button text @click="deleteMember()">Delete</Button>
             </v-card-actions>
           </v-card>
         </v-dialog>
