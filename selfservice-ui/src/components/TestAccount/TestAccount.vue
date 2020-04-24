cd55d632127996ae47b46c6f1ba547e8e7ef2c07
shabeeb <myselfshabeeb@gmail.com>
2020-04-24 12:09:38 -0700

    added name for test account

--- selfservice-ui/src/components/TestAccount/TestAccount.vue
+++ selfservice-ui/src/components/TestAccount/TestAccount.vue
@@ -40,8 +40,9 @@
                 type="text"
                 outlined
                 rows="10"
-                name="test-account"
-                id="test-account"
+                name="test-account-text"
+                id="test-account-text"
+                data-test-id="test-account-text"
               />
             </v-card>
           </v-col>
