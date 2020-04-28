8246dceae20b59d89a36eb8853c1fe3519620a3b
shabeeb <myselfshabeeb@gmail.com>
2020-04-28 16:51:59 -0700

    contact us page

---
+++ selfservice-ui/src/components/ContactUs/ContactUs.vue
@@ -0,0 +1,164 @@
+/** * ContactUs component */
+
+<template>
+  <v-card class="mx-auto outer-card">
+    <v-form ref="form" v-model="valid">
+      <v-toolbar flat class="bc-subtitle padding-0" dark>
+        <v-toolbar-title>{{ $t('contactUs.pagetitle') }}</v-toolbar-title>
+        <div class="flex-grow-1"></div>
+
+        <div class="flex-grow-1"></div>
+      </v-toolbar>
+      <v-divider></v-divider>
+
+      <v-container>
+        <v-row class="mx-4">
+          <v-col cols="12" flat>
+            <v-card flat>
+              <div class="text-left" v-html="$t('contactUs.pageinfo')"></div>
+            </v-card>
+          </v-col>
+          <v-col class="col-12" v-if="errorStatus || successStatus">
+            <Alert
+              type="error"
+              v-if="errorStatus"
+              class="alert-top text-left"
+            >{{ $t('contactUs.errorMessage') }}</Alert>
+            <Alert
+              type="success"
+              class="alert-top text-left"
+              v-if="successStatus"
+            >{{ $t('contactUs.successMessage') }}</Alert>
+          </v-col>
+          <v-col cols="8">
+            <Input
+              v-model="contactDetails.firstName"
+              :label="$t('contactUs.firstName')"
+              type="text"
+              :rules="[rules.required,  rules.maxLength(500)]"
+              data-test-id="input-app-url"
+            />
+
+            <Input
+              v-model="contactDetails.lastName"
+              :label="$t('contactUs.lastName')"
+              type="text"
+              :rules="[rules.required, rules.maxLength(500)]"
+              data-test-id="input-app-url"
+            />
+            <Input
+              v-model="contactDetails.email"
+              :label="$t('contactUs.emailAddress')"
+              type="text"
+              :rules="[rules.required, rules.email, rules.maxLength(500)]"
+              data-test-id="input-app-url"
+            />
+          </v-col>
+          <v-col cols="12" flat>
+            <v-card flat>
+              <TextArea
+                v-model="contactDetails.description"
+                :label="$t('contactUs.description')"
+                type="text"
+                outlined
+                rows="10"
+                name="test-account-text"
+                id="test-account-text"
+                data-test-id="contact-us-description"
+                :rules="[rules.required]"
+              />
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-col cols="12">
+          <v-card flat>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <Button
+                :disabled="!valid"
+                :loading="isLoading"
+                class="white--text submit-account ml-6"
+                depressed
+                @click="submitContactMessage"
+                name="btn-test-account"
+                data-test-id="btn-test-account"
+              >{{ $t('contactUs.btnSend') }}</Button>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-container>
+    </v-form>
+  </v-card>
+</template>
+<script lang="ts">
+import { Component, Vue, Watch, Prop } from 'vue-property-decorator';
+import { Getter, namespace, Action } from 'vuex-class';
+import validationRules from '@/config/validationRules';
+
+import Button from '@/Atomic/Button/Button.vue';
+import TextArea from '@/Atomic/TextArea/TextArea.vue';
+import Alert from '@/Atomic/Alert/Alert.vue';
+import Input from '@/Atomic/Input/Input.vue';
+
+import { contactDetailsSample } from '@/store/modules/ContactUs/defaults';
+
+const ContactUsModule = namespace('ContactUsModule');
+
+@Component({
+  components: {
+    Button,
+    TextArea,
+    Alert,
+    Input
+  }
+})
+export default class ContactUs extends Vue {
+  @Prop({ default: 0 })
+  public id!: number;
+
+  @ContactUsModule.Action('addContactUs')
+  public addContactUs!: any;
+  @ContactUsModule.Getter('isLoading')
+  public isLoading!: boolean;
+  @ContactUsModule.Getter('getChangeStatus')
+  public getChangeStatus!: boolean;
+
+  @ContactUsModule.Action('clearStatus') public clearStatus!: any;
+
+  private rules = validationRules;
+
+  private contactDetails: any;
+  private valid: boolean = false;
+  private successStatus: boolean = false;
+  private errorStatus: boolean = false;
+
+  public constructor() {
+    super();
+    this.contactDetails = contactDetailsSample();
+  }
+
+  @Watch('getChangeStatus')
+  private ongetChangeStatusChanged(val: any) {
+    const { successStatus, errorStatus } = val;
+    this.successStatus = successStatus;
+    this.errorStatus = errorStatus;
+  }
+
+  private submitContactMessage() {
+    this.addContactUs({
+      contactDetails: this.contactDetails
+    });
+  }
+
+  private mounted() {
+    this.clearStatus();
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './../../assets/styles/theme.scss';
+</style>
