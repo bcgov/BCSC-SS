4ff62d1562f0cc06a67b0d719c3cb943bee6d861
shabeeb <myselfshabeeb@gmail.com>
2020-05-01 14:06:06 -0700

    border color for button

--- selfservice-ui/src/Atomic/Button/Button.vue
+++ selfservice-ui/src/Atomic/Button/Button.vue
@@ -6,7 +6,7 @@ important action on your service, such as Download or Submit. */
     class="btn"
     :type="type"
     :name="name"
-    v-bind:class="getclassList()"
+    :class="getclassList()"
     :aria-disabled="disabled"
     :disabled="disabled"
     @click="click"
@@ -103,6 +103,7 @@ export default class Button extends Vue {
     }
     classToApply += this.dark !== false ? ' dark-calss' : ' normal';
     classToApply += this.disabled !== false ? ' disabled' : '';
+    console.log('classToApply', classToApply);
     return classToApply;
   }
 }
@@ -162,9 +163,13 @@ export default class Button extends Vue {
   color: $BCgovFontColorInvertedDark !important;
   opacity: 1;
 }
+.v-application .primary {
+  border: none;
+  border: 2px solid $BCgovBlue5 !important;
+}
 
 .primary {
-  border: none;
+  border: 2px solid $BCgovBlue5 !important;
   // background-color: #003366;
   border-radius: 4px;
   color: white;
@@ -207,5 +212,6 @@ export default class Button extends Vue {
 }
 .primary.theme--light.v-btn.v-btn--disabled:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined) {
   background-color: $BCgovBlue5 !important;
+  color: white !important;
 }
 </style>
