f4d7398851a2b3a7d1c3259ddffe38cd8b965a1f
shabeeb <myselfshabeeb@gmail.com>
2020-05-07 10:09:18 -0700

    updated test case

--- selfservice-ui/jest.setup.ts
+++ selfservice-ui/jest.setup.ts
@@ -4,3 +4,19 @@ import Vuex from 'vuex';
 
 Vue.use(Vuetify);
 Vue.use(Vuex);
+
+(global as any).IntersectionObserver = class IntersectionObserver {
+  constructor() {} // tslint:disable-line
+
+  private observe() {
+    return null;
+  }
+
+  private disconnect() {
+    return null;
+  }
+
+  private unobserve() {
+    return null;
+  }
+};
