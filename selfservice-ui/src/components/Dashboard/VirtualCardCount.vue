1c7e786177f6739cb54ebbffbc38e859cb89f6bf
shabeeb <myselfshabeeb@gmail.com>
2020-05-13 12:46:48 -0700

    button issue in dashboard

--- selfservice-ui/src/components/Dashboard/VirtualCardCount.vue
+++ selfservice-ui/src/components/Dashboard/VirtualCardCount.vue
@@ -11,10 +11,10 @@
     <v-divider></v-divider>
     <v-container>
       <v-row class="ma-2">
-        <v-col cols="12" md="8" class="text-center">
+        <v-col cols="12" md="7" class="text-center">
           <RemaingAccounts />
         </v-col>
-        <v-col cols="12" md="4">
+        <v-col cols="12" md="5">
           <div class="remaining-acc-btn">
             <Button
               class="white--text mt-md-4 "
@@ -54,7 +54,7 @@ export default class VirtualCardCount extends Vue {}
 
 .remaining-acc-btn {
   display: inline;
-  margin-top: 15px;
+
   @include lg {
     float: right;
   }
