0bb80bdbfa4d02d20ca3d4f133a49c6408f359c6
shabeeb <myselfshabeeb@gmail.com>
2020-05-13 11:14:01 -0700

    dev name change

--- selfservice-ui/tests/unit/components/TestAccount/AddTestAccount.spec.ts
+++ selfservice-ui/tests/unit/components/TestAccount/AddTestAccount.spec.ts
@@ -1,4 +1,4 @@
-import { shallowMount, mount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import AddTestAccount from '@/components/TestAccount/AddTestAccount.vue';
 import Vuetify from 'vuetify';
 import Vuex from 'vuex';
