4ffa2e27f48392725cfd3568dc2e4862fb29f938
ttejassvi <tanya.tejassvi@aot-technologies.com>
2020-05-28 19:28:50 -0700

    Variable for wait time, Checking in latest code

--- selfservice-ui/cypress/integration/examples/BC_Analyst_Login_TestAccounts.spec.js
+++ selfservice-ui/cypress/integration/examples/BC_Analyst_Login_TestAccounts.spec.js
@@ -3,51 +3,53 @@
 import "cypress-keycloak-commands";
 describe('BC Dev Login Test for Analyst', function()
 {
+    //Declaring a variable to change wait time in between the test for recording videos. Time is in milliseconds
+    const time_ms = 500
     it('Visit the BC Dev Page and Login as Analyst', function(){
         cy.log("Visiting the BC Services Card [DEV] URL")
         cy.visit('https://selfservice-dev.pathfinder.gov.bc.ca/')
-        cy.wait(5000)
+        cy.wait(time_ms)
         //Find the Login button
         cy.log("Searching for the Login button")
         cy.contains('Login').click()
-        cy.wait(5000)
+        cy.wait(time_ms)
         //Make sure to logout of any previous Keycloak sessions
         cy.log("Logging out of previous Keycloak session")
         cy.kcLogout();
         cy.log("Fetching details from Json file and validating with Keycloak")
-        cy.wait(5000)
+        cy.wait(time_ms)
         //Logging in as Analyst via Keycloak (Picks up values from users/analyst.json)
         cy.log("Log In as Analyst")
         cy.kcLogin("analyst");
-        cy.wait(5000)
+        cy.wait(time_ms)
         cy.log("Log in successful and user is directed to Dashboard")
         cy.visit("https://selfservice-dev.pathfinder.gov.bc.ca/dashboard");
-        cy.wait(5000)
+        cy.wait(time_ms)
         cy.log("Analyst can view Remaining test accounts right above the dashboard")
         cy.log("Displays the remaining test accounts")
         cy.contains('test accounts remaining')
         //Click on Load Test Accounts
         cy.log("Click on Manage Test Accounts")
         cy.get('[data-test-id="btn-create-test-account"]').click()
-        cy.wait(5000)
+        cy.wait(time_ms)
         cy.log("Redirected to Add Test Account Page")
-        cy.wait(10000)
+        cy.wait(time_ms)
         cy.visit('https://selfservice-dev.pathfinder.gov.bc.ca/add-test-account')
-        cy.wait(5000)
+        cy.wait(time_ms)
         cy.log("Paste csv contents in text area")
-        cy.wait(5000)
+        cy.wait(time_ms)
         cy.contains('Paste your CSV in this text box')
-        cy.wait(5000)
-      
-        cy.get('#test-account-text').type('SS4BPS201,98901,ONE,SS4BPS Felecia,F,4732 Easy Street,Highlands BC V9B 3V9,V9B 3V9,1998-04-30{enter}')
-        cy.wait(5000)
-        cy.get('#test-account-text').type('SS4BPS202,98902,TWO,SS4BPS Benjamin ,M,308-2464 Crimson Vale,Penticton BC V2A 5N1,V2A 5N1,2000-11-18{enter}')
-        cy.wait(5000)
-        cy.get('#test-account-text').type('SS4BPS203,98903,THREE,SS4BPS Tyisha,F,3853 Lazy Road,Alexis Creek BC V0L 3I3,V0L 3I3,1999-03-19{enter}')
-        cy.wait(5000)
+        cy.wait(time_ms)
+        //Enter a set of Test Accounts to test
+        cy.get('#test-account-text').type('<Test Account1 and details comma separated>{enter}')
+        cy.wait(time_ms)
+        cy.get('#test-account-text').type('<Test Account2 and details comma separated>{enter}')
+        cy.wait(time_ms)
+        cy.get('#test-account-text').type('<Test Account3 and details comma separated>{enter}')
+        cy.wait(time_ms)
         cy.log("Click : Import")
         cy.get('[data-test-id="btn-test-account"]').click()
-        cy.wait(1000)
+        cy.wait(time_ms)
         cy.log("Should display number of test accounts loaded or skipped")
         cy.log("Test Complete")
     })
--- selfservice-ui/cypress/integration/examples/BC_Keycloak_Analyst.spec.js
+++ selfservice-ui/cypress/integration/examples/BC_Keycloak_Analyst.spec.js
@@ -1,24 +1,25 @@
 // Visit a web page
 import "cypress-keycloak-commands";
 describe('BC Keycloak Test', function()
-{
+{   
+    const time_ms = 500
     it('Visit the BC Dev Page and Login as Analyst', function()
     {
         cy.log("Visiting the BC Services Card [DEV] URL")
         cy.visit('https://selfservice-dev.pathfinder.gov.bc.ca/')
-        cy.wait(10000)
+        cy.wait(time_ms)
         cy.log("Searching for the Login Button")
         cy.contains('Login').click()
-        cy.wait(5000)
+        cy.wait(time_ms)
         cy.log("Fetching details from Json file and validating with Keycloak")
         cy.kcLogout();
         //Testing Login for analyst
         cy.log("Logging in as Analyst")
         cy.kcLogin("analyst");
-        cy.wait(5000)
+        cy.wait(time_ms)
         cy.log("Successfully logged in and directed to Dashboard")
         cy.visit("https://selfservice-dev.pathfinder.gov.bc.ca/dashboard");
-        cy.wait(5000)
+        cy.wait(time_ms)
         cy.log("Test Complete")
     })
 })
